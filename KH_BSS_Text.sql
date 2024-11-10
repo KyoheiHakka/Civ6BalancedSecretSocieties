0cceafdd0ab180445c70ca41f0d210ae35ddf180
KyoheiHakka <fmilkh49@i.softbank.jp>
2024-11-10 16:45:26 +0900

    できたー

--- KH_BSS_Text.sql
+++ KH_BSS_Text.sql
@@ -1,9 +1,7 @@
-<!-- TODO: 日本語のみでいいから更新 -->
-INSERT INTO ModifierStrings (ModifierId, Context, Text) VALUES
-('MODIFIER_KH_VAMPIRE_POST_COMBAT_YIELD_GOLD', 'Preview', 'LOC_KH_ABILITY_VAMPIRE_POST_COMBAT_YIELD_GOLD_DESCRIPTION'),
-('MODIFIER_KH_VAMPIRE_POST_COMBAT_YIELD_SCIENCE', 'Preview', 'LOC_KH_ABILITY_VAMPIRE_POST_COMBAT_YIELD_SCIENCE_DESCRIPTION'),
-('MODIFIER_KH_VAMPIRE_POST_COMBAT_YIELD_CULTURE', 'Preview', 'LOC_KH_ABILITY_VAMPIRE_POST_COMBAT_YIELD_CULTURE_DESCRIPTION');
-
+-- RevealPromotions
+-- Author: jason
+-- DateCreated: 4/19/2021 11:12:18 PM
+--------------------------------------------------------------
 
 UPDATE LocalizedText
 SET Text = '自然遺産を発見すると解除される。レイライン資源の位置が明らかになる。レイラインはすべての専門区域に大きな隣接ボーナスをもたらす。全てのキャンパスは[ICON_HOUSING]+2と[ICON_AMENITIES]+1を得る。'
@@ -14,6 +12,7 @@ SET Text = '現在の政府に経済政策スロット+1。都市国家に[ICON_
 WHERE Tag = 'LOC_GOVERNOR_PROMOTION_OWLS_OF_MINERVA_1_DESCRIPTION';
 
 INSERT OR IGNORE INTO LocalizedText (Language, Tag, Text) VALUES
+('ja_JP', 'LOC_Ability_KH_VAMPIRE_TRAIT_POST_COMBAT_YIELD_DESCRIPTION', '戦闘に勝利すると、そのユニットの基本 [ICON_Strength] 戦闘力の50%に相当する [ICON_Gold]ゴールド、25%に相当する[ICON_Science]科学と[ICON_Culture]文化を得る (速度オンラインで)'),
 ('ja_JP', 'LOC_KH_ABILITY_VAMPIRE_POST_COMBAT_YIELD_GOLD_DESCRIPTION', '戦闘に勝利すると、そのユニットの基本 [ICON_Strength] 戦闘力の50%に相当する [ICON_Gold] ゴールドを得る (速度オンラインで)'),
 ('ja_JP', 'LOC_KH_ABILITY_VAMPIRE_POST_COMBAT_YIELD_SCIENCE_DESCRIPTION', '戦闘に勝利すると、そのユニットの基本 [ICON_Strength] 戦闘力の25%に相当する [ICON_Science] 科学を得る (速度オンラインで)'),
 ('ja_JP', 'LOC_KH_ABILITY_VAMPIRE_POST_COMBAT_YIELD_CULTURE_DESCRIPTION', '戦闘に勝利すると、そのユニットの基本 [ICON_Strength] 戦闘力の25%に相当する [ICON_Culture] 文化を得る (速度オンラインで)'),
@@ -21,4 +20,4 @@ INSERT OR IGNORE INTO LocalizedText (Language, Tag, Text) VALUES
 ('ja_JP', 'LOC_KH_MINERVA_TRADE_BONUS_YIELD_PRODUCTION_DESCRIPTION', '国外への[ICON_TradeRoute]交易路から追加の[ICON_PRODUCTION]生産力+2が得られる。'),
 ('ja_JP', 'LOC_KH_MINERVA_TRADE_BONUS_YIELD_GOLD_DESCRIPTION', '国外への[ICON_TradeRoute]交易路から追加の[ICON_GOLD]ゴールド+2が得られる。'),
 ('ja_JP', 'LOC_KH_HERMES_CAMPUS_AMENITY_DESCRIPTION', 'キャンパスは[ICON_AMENITIES]快適性+1を得る'),
-('ja_JP', 'LOC_KH_HERMES_CAMPUS_HOUSING_DESCRIPTION', 'キャンパスは[ICON_HOUSING]住宅+2を得る);
+('ja_JP', 'LOC_KH_HERMES_CAMPUS_HOUSING_DESCRIPTION', 'キャンパスは[ICON_HOUSING]住宅+2を得る');
